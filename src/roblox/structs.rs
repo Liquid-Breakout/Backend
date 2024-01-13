f15f8a5189dd860c0c10ade54ee66c99c99e292d
cutymeoRBLX <hkhanhnam81@gmail.com>
2024-01-13 15:13:32 +0700

    serde renames

--- src/roblox/structs.rs
+++ src/roblox/structs.rs
@@ -25,32 +25,47 @@ pub enum CreatorType {
 #[derive(
 Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default, Serialize, Deserialize,
 )]
-#[serde(rename_all = "PascalCase")]
 pub struct Creator {
+    #[serde(rename = "Id")]
     pub id: u64,
+    #[serde(rename = "HasVerifiedBadge")]
     pub has_verified_badge: bool,
+    #[serde(rename = "CreatorType")]
     pub creator_type: CreatorType,
-    pub creator_target_id: u64,
+    #[serde(rename = "CreatorTargetId")]
+    pub target_id: u64,
+    #[serde(rename = "Name")]
     pub name: String,
 }
 #[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "PascalCase")]
 pub struct ItemDetails {
+    #[serde(rename = "Id")]
     pub id: u64,
+    #[serde(rename = "TargetId")]
     pub target_id: u64,
+    #[serde(rename = "ProductId")]
     pub product_id: u64,
+    #[serde(rename = "AssetTypeId")]
     pub asset_type_id: Option<AssetType>,
+    #[serde(rename = "Name")]
     pub name: String,
+    #[serde(rename = "Description")]
     pub description: String,
+    #[serde(rename = "Creator")]
     pub creator: Creator,
+    #[serde(rename = "PriceInRobux")]
     pub price_in_robux: Option<u64>,
+    #[serde(rename = "CollectibleItemId")]
     pub collectible_item_id: Option<String>,
+    #[serde(rename = "IsForSale")]
     pub is_for_sale: Option<bool>,
+    #[serde(rename = "IsPublicDomain")]
     pub is_public_domain: Option<bool>,
 }
 #[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
 pub struct AssetPurchaseReq {
+    #[serde(rename = "expectedCurrency")]
     pub expected_currency: u64,
+    #[serde(rename = "expectedPrice")]
     pub expected_price: u64
 }
