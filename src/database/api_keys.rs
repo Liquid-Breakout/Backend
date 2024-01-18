1eafe84e833e3e07db004f35366829d2d99780b0
cutymeoRBLX <hkhanhnam81@gmail.com>
2024-01-18 16:05:20 +0700

    fix for real

--- src/database/api_keys.rs
+++ src/database/api_keys.rs
@@ -18,7 +18,7 @@ pub struct ApiKey {
 impl Backend {
     pub(super) async fn generate_api_key(&self) -> Result<String, Box<dyn std::error::Error>> {
         let database = self.get_database();
-        let api_keys_collection: Collection<ApiKey> = database.collection("apiKey");
+        let api_keys_collection: Collection<ApiKey> = database.collection("apikeys");
         
         // my reaction when rust
         let api_key_generator: IDConverter = IDConverter::new(
@@ -33,7 +33,7 @@ impl Backend {
     pub async fn find_api_key_entry(&self, api_key: &str) -> Result<Option<ApiKey>, Box<dyn std::error::Error>> {
         let database = self.get_database();
 
-        let api_keys_collection: Collection<ApiKey> = database.collection("apiKey");
+        let api_keys_collection: Collection<ApiKey> = database.collection("apikeys");
 
         let result = api_keys_collection.find_one(
             doc! { 
@@ -57,7 +57,7 @@ impl Backend {
     pub async fn create_api_key_entry(&self) -> Result<(), Box<dyn std::error::Error>> {
         let database = self.get_database();
         
-        let api_keys_collection: Collection<ApiKey> = database.collection("apiKey");
+        let api_keys_collection: Collection<ApiKey> = database.collection("apikeys");
         let new_api_key = self.generate_api_key().await?;
 
         let doc = ApiKey {
@@ -74,7 +74,7 @@ impl Backend {
     pub async fn search_api_key_entries_with_roblox_id(&self, roblox_id: u64) -> Result<Option<ApiKey>, Box<dyn std::error::Error>> {
         let database = self.get_database();
         
-        let api_keys_collection: Collection<ApiKey> = database.collection("apiKey");
+        let api_keys_collection: Collection<ApiKey> = database.collection("apikeys");
 
         let result = api_keys_collection.find_one(
             doc! { 
@@ -89,7 +89,7 @@ impl Backend {
     pub async fn search_api_key_entries_with_discord_id(&self, discord_id: u64) -> Result<Option<ApiKey>, Box<dyn std::error::Error>> {
         let database = self.get_database();
         
-        let api_keys_collection: Collection<ApiKey> = database.collection("apiKey");
+        let api_keys_collection: Collection<ApiKey> = database.collection("apikeys");
 
         let result = api_keys_collection.find_one(
             doc! { 
